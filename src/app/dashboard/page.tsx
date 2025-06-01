128d78f9cc871a8b1001ccc8b9542c4cab518615
Emmerik Boy <emmerikboy@gmail.com>
2025-06-01 15:38:14 +0200

    Initial commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-white">
+      <h1 className="text-4xl font-bold text-green-600">Heisan hopsan</h1>
+    </main>
+  );
+}
